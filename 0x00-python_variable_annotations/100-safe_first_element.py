e58941993134bde8e42591690b1420c0dc8e2f1b
shikoliamos31 <amosshikoli@gmail.com>
2024-07-04 22:54:08 +0300

    annotations

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,18 +1,11 @@
-#!/usr/bin/env python3
-"""
-A module that has a type-annotated function
-using a TypeVar
-"""
-from typing import Any, Mapping, Optional, TypeVar, Union
-T = TypeVar('T')
+!/usr/bin/env python3
+""" Module documentation """
+from typing import Sequence, Any, Union
 
 
-def safely_get_value(dct: Mapping, key: Any,
-                     default: Union[T, None] = None) -> Union[Any, T]:
-    """
-    Takes in a Mapping, TypeVar and returns a union of Any and TypeVar
-    """
-    if key in dct:
-        return dct[key]
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """Func doc"""
+    if lst:
+        return lst[0]
     else:
-        return default
+        return None
