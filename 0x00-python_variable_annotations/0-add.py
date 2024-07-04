53057bddc02641fa2256ad126c3013a46cc97fc5
shikoliamos31 <amosshikoli@gmail.com>
2024-07-04 12:30:46 +0300

    Basic annotations - add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
+'''Modules of task-0.
+'''
+
 
 def add(a: float, b: float) -> float:
-    """Add two floats and return their sum."""
+    '''Adds two floating-point numbers.
+    '''
     return a + b
