96c2f39853e01a7deaaf9fea95420da06603489f
shikoliamos31 <amosshikoli@gmail.com>
2024-07-04 22:59:27 +0300

    annotations

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,10 +1,16 @@
-!/usr/bin/env python3
-""" Module documentation """
-from typing import Sequence, Any, Union
+#!/usr/bin/env python3
+"""Duck typing - first element of a sequence
+"""
+from typing import Sequence, Any, Union, Optional
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
-    """Func doc"""
+    """Augment the code with correct duck-typed annotations
+    args:
+        lst: Sequence[Any]
+    return:
+        Union[Any, NoneType]
+    """
     if lst:
         return lst[0]
     else:
