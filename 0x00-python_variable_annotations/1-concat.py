8bc46342b89f29452d5e6190b2f955c68fd01177
shikoliamos31 <amosshikoli@gmail.com>
2024-07-05 22:09:22 +0300

    Basic annotations - concat

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-"""This module concatenate two strings.
-"""
+'''Module for task 1.
+'''
+
 
 def concat(str1: str, str2: str) -> str:
-    """
-    Concatenates two strings.
-    """
+    '''Concatenates two strings.
+    '''
     return str1 + str2
