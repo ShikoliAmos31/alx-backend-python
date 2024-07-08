62228b5296e729b79ea7963710afcd8d19fba821
shikoliamos31 <amosshikoli@gmail.com>
2024-07-08 17:37:03 +0300

    The async

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
-'''Task 4's module.
-'''
-import asyncio
+"""execute multiple coroutines at the same time with async"""
+
 from typing import List
+from asyncio import gather
 
-
+# import wait_random from previous file
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    '''Executes task_wait_random n times.
-    '''
-    wait_times = await asyncio.gather(
-        *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
-    )
-    return sorted(wait_times)
+    """run an async function n times with max_delay returning list of delays"""
+    list_of_delays: float = []
+    coroutines = [task_wait_random(max_delay) for _ in range(n)]
+    results = await gather(*coroutines)
+    list_of_delays.extend(results)
+
+    return sorted(list_of_delays)
